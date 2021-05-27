47fc60e988bf27aa1a0f22173206476afdd0b95e
Jose Aguilar <jhagui7464@gmail.com>
2021-05-27 11:53:21 -0500

    added

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -1,6 +1,5 @@
 module.exports = app => {
     const users = require("../controllers/user.controller.js");
-
     app.get("/users/:username/:password", users.findOne);
     app.get("/userTables/:userID", users.findOrders);
     app.get("/userTables/done/:userID", users.findDoneOrders);
